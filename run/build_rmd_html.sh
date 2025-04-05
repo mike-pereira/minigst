74ac8e7d794a343d62d7b618d553cf9483ce028d
Fabien Ors <fabien.ors@minesparis.psl.eu>
2025-04-05 19:10:18 +0200

    Exit CI when TP script fails

--- run/build_rmd_html.sh
+++ run/build_rmd_html.sh
@@ -5,31 +5,10 @@ out_dir=$2
 runner=$3
 echo "Processing $in_dir to $out_dir"
 
-if [ -d $out_dir ]
-then
-  if [ -z "$(ls -A $out_dir)" ]
-  then
-    echo "$out_dir exists and is empty."
-  else
-    echo "$out_dir exists and full of stuff."
-  fi
-  while true; do
-    read -p "Do you want to continue (Y/N) ?" yn
-
-    case $yn in 
-	y ) break;;
-	Y ) break;;
-	n ) exit;;
-	N ) exit;;
-	* ) ;;
-    esac
-  done
-fi
-
 if [ ! -d $in_dir ]
 then
   echo "$in_dir doesn't exist. Abort!"
-  exit
+  exit -1
 fi
 
 mkdir -p $out_dir
@@ -39,6 +18,12 @@ for fsc in $flist
 do
   echo "  Processing $fsc"
   R CMD BATCH --no-save --no-restore "--args $fsc $out_dir html" $runner
+  if [ $? -ne 0 ]
+  then
+    echo "Error while executing $fsc"
+    exit -1
+  fi
+  cat run_test_rmd.Rout
 done
 
 echo "Done"
